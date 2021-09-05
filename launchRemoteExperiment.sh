93cb513791b4b621616f405d769e17a03d62ac0b
monte97 <francesco.montelli1997@gmail.com>
2021-09-05 18:13:33 +0000

    working on launch experiment

--- launchRemoteExperiment.sh
+++ launchRemoteExperiment.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
+FIWARE_IP="137.204.74.57"
+DRACO_IP="137.204.74.56"
 
-NUM_58=$1
-NUM_57=$2
+if [ -f .env ]; then
+  export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
+fi
+if [ -f kafka-docker/.env ]; then
+  export $(echo $(cat kafka-docker/.env | sed 's/#.*//g'| xargs) | envsubst)
+fi
 
-TERM_TIME=$3
-SUB_NUM=$4
-HOW_MANY_MESSAGES=$5
-HOW_OFTEN_SPEEDUP=$6
-SPEEDUP=$7
-PAYLOAD_KB=$8
 
-START_58=0
-START_57=7777
-EXP_NAME=${NUM_58}_${NUM_57}_${TERM_TIME}_${HOW_MANY_MESSAGES}_${SUB_NUM}_${HOW_OFTEN_SPEEDUP}_${SPEEDUP}_${PAYLOAD_KB}_`date +"%G%m%d_%H%M"`
+NUM_DEVICE=$1
+DEVICE_TIME=$2
+SUB_NUM=$3
+HOW_MANY_MESSAGES=$4
+HOW_OFTEN_SPEEDUP=$5
+SPEEDUP=$6
+PAYLOAD_KB=$7
+
+EXP_NAME=${NUM_DEVICE}_${TERM_TIME}_${HOW_MANY_MESSAGES}_${SUB_NUM}_${HOW_OFTEN_SPEEDUP}_${SPEEDUP}_${PAYLOAD_KB}_`date +"%G%m%d_%H%M"`
 echo ${EXP_NAME}
 
-#avviare fiware
-echo "setup fiware"
-./setupFiware.sh
-
 ROOT="/home/fmontelli"
-CODE_FOLDER="demoweb2_performance_websplit"
+CODE_FOLDER="tmp/fiware-performance2"
 
+#avviare fiware
+echo "setup fiware on ${FIWARE_IP}"
+ssh fmontelli@${FIWARE_IP}  docker-compose -f ${ROOT}/${CODE_FOLDER}/docker-compose-fiware.yml up --build &>/dev/null &
 
-echo "launch webserver"
-ssh fmontelli@137.204.74.56  docker-compose -f ${ROOT}/${CODE_FOLDER}/webserver/docker-compose.yml up --build &>/dev/null &
-sleep 1m
+echo "setup draco on ${DRACO_IP}"
+ssh fmontelli@${DRACO_IP} docker-compose -f ${ROOT}/${CODE_FOLDER}/docker-compose-draco.yml up --build &>/dev/null &
+
 
 echo "setup subscriptions"
 #una sottoscrizione viene già creata dallo script di setup
@@ -34,64 +39,42 @@ for ((i=0; i<${SUB_NUM}; i++))
 do
   echo "sub ${i}"
   curl -iX POST \
-    --url 'http://localhost:8082/v2/subscriptions' \
+    --url 'http://${FIWARE_UP}:${ORION_PORT_EXT}/v2/subscriptions' \
     --header 'Content-Type: application/json' \
     --header 'fiware-service: openiot' \
     --header 'fiware-servicepath: /' \
     --data '{
     "description": "Notify me when any Thermometer changes state",
     "subject": {
-    "entities": [{"idPattern": ".*","type": "Thermometer"}],
+    "entities": [{"idPattern": ".*","type": "Device"}],
     "condition": {
-      "attrs": ["Status", "Temperature", "Timestamp"]
+      "attrs": ["Status", "Payload", "Timestamp"]
     }
     },
     "notification": {
     "http": {
-      "url": "http://137.204.74.56:8080/api/fiware/notification/thermometer"
+      "url": "http://${DRACO_IP}:${DRACO_WS_PORT}/v2/notify"
     },
     "attrsFormat" : "keyValues"
     }
   }' &>/dev/null
 done
-
-
-
 echo "Finish subscriptions creation"
-#mettre in esecuzione esperimento remoto
-echo "launch on 58"
-ssh fmontelli@137.204.74.58 ${ROOT}/${CODE_FOLDER}/createDevices.sh ${START_58} ${NUM_58} ${TERM_TIME} ${SUB_NUM} ${HOW_MANY_MESSAGES} ${HOW_OFTEN_SPEEDUP} ${SPEEDUP} ${PAYLOAD_KB} ${EXP_NAME} $>/dev/null &
 
-echo "launch on 57"
-ssh fmontelli@137.204.74.57 ${ROOT}/${CODE_FOLDER}/createDevices.sh ${START_57} ${NUM_57} ${TERM_TIME} ${SUB_NUM} ${HOW_MANY_MESSAGES} ${HOW_OFTEN_SPEEDUP} ${SPEEDUP} ${PAYLOAD_KB} ${EXP_NAME} $>/dev/null &
-
-sleep 1m
-#ottenere conteggio
-count_58=$(($(ssh fmontelli@137.204.74.58 "docker ps | wc -l")-1))
-#attendere che il conteggio arrivi al valore desiderato
-while [ "${count_58}" != "${NUM_58}" ]
+echo "launch remote device on ${DEVICE_IP}"
+ssh fmontelli@${DEVICE_IP} ${ROOT}/${CODE_FOLDER}/supportScript/createDevices.sh ${START_58} ${NUM_58} ${TERM_TIME} ${SUB_NUM} ${HOW_MANY_MESSAGES} ${HOW_OFTEN_SPEEDUP} ${SPEEDUP} ${PAYLOAD_KB} ${EXP_NAME} $>/dev/null &
+echo "wait launch on ${DEVICE_IP} completion"
+count_device=$(($(ssh fmontelli@${DEVICE_IP} "docker ps | wc -l")-1))
+while [ "${count_device}" != "${NUM_58}" ]
 do
-  count_58=$(($(ssh fmontelli@137.204.74.58 "docker ps | wc -l")-1))
-  sleep 1m
+  count_device=$(($(ssh fmontelli@${DEVICE_IP} "docker ps | wc -l")-1))
+  sleep 10s
 done
+echo "launch on ${DEVICE_IP} complete"
 
-echo "count_58 ok"
-
-#ottenere conteggio
-sleep 1m
-count_57=$(($(ssh fmontelli@137.204.74.57 "docker ps | wc -l")-1))
-#attender0e che il conteggio arrivi al valore desiderato
-while [ "${count_57}" != "${NUM_57}" ]
-do
-  count_57=$(($(ssh fmontelli@137.204.74.57 "docker ps | wc -l")-1))
-  sleep 1m
-done
-
-echo "count_57 ok"
-
-echo "on"
+echo "turn on all the devices"
 #dare on  a tutti i device, on deve essere mandato ad OCB
-for ((i=${START_58}; i<${START_58}+${NUM_58}; i++))
+for ((i=0; i<${NUM_DEVICE}; i++))
 do
   echo ${i}
   curl \
@@ -101,7 +84,7 @@ do
     --retry-delay 0 \
     --retry-max-time 40 \
     -iX PATCH \
-    --url "http://137.204.74.59:8082/v2/entities/urn:ngsi-ld:thermometer:${i}/attrs" \
+    --url "http://${FIWARE_IP}:${ORION_PORT_EXT}/v2/entities/urn:ngsi-ld:device:${i}/attrs" \
     --header 'Content-Type: application/json' \
     --header 'fiware-service: openiot' \
     --header 'fiware-servicepath: /' \
@@ -113,62 +96,27 @@ do
   }' &
 done
 
-echo "on"
-#dare on  a tutti i device, on deve essere mandato ad OCB
-for ((i=${START_57}; i<${START_57}+${NUM_57}; i++))
-do
-  echo ${i}
-  curl \
-    --max-time 10 \
-    --connect-timeout 2 \
-    --retry 5 \
-    --retry-delay 0 \
-    --retry-max-time 40 \
-    -iX PATCH \
-    --url "http://137.204.74.59:8082/v2/entities/urn:ngsi-ld:thermometer:${i}/attrs" \
-    --header 'Content-Type: application/json' \
-    --header 'fiware-service: openiot' \
-    --header 'fiware-servicepath: /' \
-    --data '{
-      "on": {
-        "type": "command",
-        "value": ""
-      }
-  }' &
-done
-
-echo "wait completion 58"
-count_58=$(($(ssh fmontelli@137.204.74.58 "docker ps | wc -l")-1))
+echo "wait completion"
+count_device=$(($(ssh fmontelli@${DEVICE_IP} "docker ps | wc -l")-1))
 while [ "${count_58}" != "0" ]
 do
-  count_58=$(($(ssh fmontelli@137.204.74.58 "docker ps | wc -l")-1))
-  sleep 1m
+  count_device=$(($(ssh fmontelli@${DEVICE_IP} "docker ps | wc -l")-1))
+  sleep 10s
 done
-
-echo "wait completion 57"
-count_57=$(($(ssh fmontelli@137.204.74.57 "docker ps | wc -l")-1))
-while [ "${count_57}" != "0" ]
-do
-  count_57=$(($(ssh fmontelli@137.204.74.57 "docker ps | wc -l")-1))
-  sleep 1m
-done
+echo "completed"
 
 echo "wait before stop"
-#sleep 15m
+sleep 10m
 
-echo "stop remote experiment on 58"
-ssh fmontelli@137.204.74.58 ${ROOT}/${CODE_FOLDER}/stopExperiment.sh &>/dev/null
+echo "stop draco on ${DRACO_IP}"
+ssh fmontelli@${DRACO_IP}  docker-compose -f ${ROOT}/${CODE_FOLDER}/docker-compose-draco.yml up --build &>/dev/null &
 
-echo "stop remote experiment on 57"
-ssh fmontelli@137.204.74.57 ${ROOT}/${CODE_FOLDER}/stopExperiment.sh &>/dev/null
+echo "stop fiware on ${FIWARE_IP}"
+ssh fmontelli@${FIWARE_IP}  docker-compose -f ${ROOT}/${CODE_FOLDER}/docker-compose-fiware.yml up --build &>/dev/null &
 
-echo "stop local experiment"
-./stopExperiment.sh &>/dev/null
 
-echo "stop one more time"
-./stopAll.sh &>/dev/null
+
 echo "download files"
-
 LOGS_FOLDER="logs"
 mkdir ${LOGS_FOLDER}/${EXP_NAME}
 mkdir ${LOGS_FOLDER}/${EXP_NAME}/"devices"
