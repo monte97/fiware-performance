64c18875f030048444ec8b2475bafb613f4609f7
monte97 <francesco.montelli1997@gmail.com>
2021-09-06 12:56:34 +0000

    script to stop all running instances

--- supportScripts/stopAll.sh
+++ supportScripts/stopAll.sh
@@ -4,10 +4,10 @@ if [ -f .env ]; then
 fi
 
 echo "stop fiware on ${FIWARE_IP}"
-ssh fmontelli@${FIWARE_IP}  docker-compose -f ${ROOT}/${CODE_FOLDER}/docker-compose-fiware.yml up --build &>/dev/null &
+ssh fmontelli@${FIWARE_IP}  docker-compose -f ${ROOT}/${CODE_FOLDER}/docker-compose-fiware.yml down
 
 echo "stop draco on ${DRACO_IP}"
-ssh fmontelli@${DRACO_IP}  docker-compose -f ${ROOT}/${CODE_FOLDER}/docker-compose-draco.yml up --build &>/dev/null &
+ssh fmontelli@${DRACO_IP}  docker-compose -f ${ROOT}/${CODE_FOLDER}/docker-compose-draco.yml down
 
 echo "stop devices on ${DEVICE_IP}"
 docker ps --filter name=term* --filter status=running -aq | xargs docker stop
