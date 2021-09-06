53e8d7170152f6dbbc27e923c078ee5d82c7c8ad
monte97 <francesco.montelli1997@gmail.com>
2021-09-06 16:30:30 +0000

    execut stop device on the correct device

--- supportScripts/stopAll.sh
+++ supportScripts/stopAll.sh
@@ -10,5 +10,5 @@ echo "stop draco on ${DRACO_IP}"
 ssh fmontelli@${DRACO_IP}  docker-compose -f ${ROOT}/${CODE_FOLDER}/docker-compose-draco.yml down
 
 echo "stop devices on ${DEVICE_IP}"
-docker ps --filter name=device* --filter status=running -aq | xargs docker stop
-docker rm $(docker container ls -aq --filter name=device*) -f
+ssh fmontelli@${DEVICE_IP} docker ps --filter name=device* --filter status=running -aq | xargs docker stop
+ssh fmontelli@${DEVICE_IP} docker rm $(docker container ls -aq --filter name=device*) -f
