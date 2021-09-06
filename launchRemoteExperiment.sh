5a33163c840b6059bb98b06584384fb3f43b9042
monte97 <francesco.montelli1997@gmail.com>
2021-09-06 07:32:42 +0000

    add wait for it

--- launchRemoteExperiment.sh
+++ launchRemoteExperiment.sh
@@ -24,9 +24,19 @@ CODE_FOLDER="tmp/fiware-performance2"
 #avviare fiware
 echo "setup fiware on ${FIWARE_IP}"
 ssh fmontelli@${FIWARE_IP}  docker-compose -f ${ROOT}/${CODE_FOLDER}/docker-compose-fiware.yml up --build &>/dev/null &
+echo "wait for seutp completion"
+./supportScripts/wait-for-it.sh ${FIWARE_IP}:${ZOOKEEPER_EXT_PORT} --timeout=480 -- echo "zookeeper is up"
+./supportScripts/wait-for-it.sh ${FIWARE_IP}:${KAFKA_EXT_PORT} --timeout=480 -- echo "kafka is up"
+./supportScripts/wait-for-it.sh ${FIWARE_IP}:${MOSQUITTO_PORT_EXT} --timeout=480 -- echo "mosquitto is up"
+./supportScripts/wait-for-it.sh ${FIWARE_IP}:${ORION_PORT_EXT} --timeout=480 --echo "orion is up"
+./supportScripts/wait-for-it.sh ${FIWARE_IP}:${IOTA_NORTH_PORT} --timeout=480 --echo "iota is up"
+echo "setup fiware on ${FIWARE_IP} complete"
 
 echo "setup draco on ${DRACO_IP}"
+echo "wait for completion"
 ssh fmontelli@${DRACO_IP} docker-compose -f ${ROOT}/${CODE_FOLDER}/docker-compose-draco.yml up --build &>/dev/null &
+./supportScripts/wait-for-it.sh ${DRACO_IP}:${DRACO_API_PORT} --timeout=480 --echo "draco is up"
+echo "setup draco on ${DRACO_IP} complete"
 
 
 echo "setup subscriptions"
@@ -62,7 +72,7 @@ echo "launch remote device on ${DEVICE_IP}"
 ssh fmontelli@${DEVICE_IP} ${ROOT}/${CODE_FOLDER}/supportScript/createDevices.sh ${START_58} ${NUM_58} ${TERM_TIME} ${SUB_NUM} ${HOW_MANY_MESSAGES} ${HOW_OFTEN_SPEEDUP} ${SPEEDUP} ${PAYLOAD_KB} ${EXP_NAME} $>/dev/null &
 echo "wait launch on ${DEVICE_IP} completion"
 count_device=$(($(ssh fmontelli@${DEVICE_IP} "docker ps | wc -l")-1))
-while [ "${count_device}" != "${NUM_58}" ]
+while [ "${count_device}" != "${NUM_DEIVICE}" ]
 do
   count_device=$(($(ssh fmontelli@${DEVICE_IP} "docker ps | wc -l")-1))
   sleep 10s
@@ -113,16 +123,17 @@ ssh fmontelli@${FIWARE_IP}  docker-compose -f ${ROOT}/${CODE_FOLDER}/docker-comp
 
 
 
+if false
+then
 echo "download files"
 LOGS_FOLDER="logs"
 mkdir ${LOGS_FOLDER}/${EXP_NAME}
 mkdir ${LOGS_FOLDER}/${EXP_NAME}/"devices"
-mkdir ${LOGS_FOLDER}/${EXP_NAME}/"webserver"
+mkdir ${LOGS_FOLDER}/${EXP_NAME}/"kafka-consumer"
 
 ssh fmontelli@137.204.74.56 "ls -1t ${ROOT}/${CODE_FOLDER}/webserver/mylogs2/*.csv | head -1 | xargs -I{} mv {} ${ROOT}/${CODE_FOLDER}/webserver/mylogs2/${EXP_NAME}.csv"
 scp fmontelli@137.204.74.56:${ROOT}/${CODE_FOLDER}/webserver/mylogs2/${EXP_NAME}.csv ~/tesi/${CODE_FOLDER}/logs/${EXP_NAME}/webserver/ &>/dev/null
 
 #Fare la copia dei log verso questa macchina
-scp fmontelli@137.204.74.58:${ROOT}/${CODE_FOLDER}/devices/thermometerMQTT3-remoteVersion/mylogs/${EXP_NAME}/*.csv ~/tesi/${CODE_FOLDER}/logs/${EXP_NAME}/devices/ &>/dev/null
-
-scp fmontelli@137.204.74.57:${ROOT}/${CODE_FOLDER}/devices/thermometerMQTT3-remoteVersion/mylogs2/${EXP_NAME}/*.csv ~/tesi/${CODE_FOLDER}/logs/${EXP_NAME}/devices/ &>/dev/null
+scp fmontelli@${DEVICE_IP}:${ROOT}/${CODE_FOLDER}/devices/thermometerMQTT3-remoteVersion/mylogs/${EXP_NAME}/*.csv ~/tesi/${CODE_FOLDER}/logs/${EXP_NAME}/devices/
+fi
