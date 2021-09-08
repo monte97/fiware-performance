06d52a5336d6346ca1d3a168344d9795ffd6cad0
monte97 <francesco.montelli1997@gmail.com>
2021-09-08 08:55:52 +0000

    extract required data

--- pykafkaConsumer/consumer.py
+++ pykafkaConsumer/consumer.py
@@ -8,7 +8,7 @@ consumer = KafkaConsumer(
      auto_offset_reset='earliest',
      enable_auto_commit=True,
      group_id='my-group-montelli',
-     #value_deserializer=lambda x: loads(x.decode('utf-8'))
+     value_deserializer=lambda x: loads(x.decode('utf-8'))
      )
 
 print(consumer)
@@ -16,12 +16,13 @@ print(consumer)
 for message in consumer:
     if message.serialized_value_size > 10:
       print(message.offset)
-      print(message.timestamp)
-      print(message.value)
-    else:
-      print("funny")
-    #print(message)
-    #print(message.serialized_value_size)
-    #message = message.value
-    #print(message)
+      #print(message.value)
+      deviceID = message.value["id"]
+      deviceStatus = message.value["Status"]
+      kafkaTimestamp = message.timestamp
+      dracoTimestamp = message.value["DracoTimestamp"]
+      deviceTimestamp = message.value["DeviceTime"]
+      print("kafka timestamp", kafkaTimestamp)
+      print("draco timestamp", dracoTimestamp)
+      print("device timestamp", deviceTimestamp)
     print("_____")
