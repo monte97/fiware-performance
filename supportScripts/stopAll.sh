d8f6d4aa32b9b1309f1cd89df90fee1f0541dac0
monte97 <francesco.montelli1997@gmail.com>
2021-09-10 06:49:34 +0000

    fix stop all

--- supportScripts/stopAll.sh
+++ supportScripts/stopAll.sh
@@ -11,7 +11,7 @@ ssh fmontelli@${DRACO_IP}  docker-compose -f ${ROOT}/${CODE_FOLDER}/docker-compo
 
 echo "stop devices on ${DEVICE_IP}"
 ssh fmontelli@${DEVICE_IP} docker ps --filter name=device* --filter status=running -aq | xargs docker stop
-ssh fmontelli@${DEVICE_IP} docker rm $(docker container ls -aq --filter name=device*) -f
+ssh fmontelli@${DEVICE_IP} 'docker rm $(docker container ls -aq --filter name=device*) -f'
 
 echo "stop consumer on ${KAFKACONSUMER_IP}"
 ssh fmontelli@${KAFKACONSUMER_IP} docker-compose -f ${ROOT}/${CODE_FOLDER}/docker-compose-pythonconsumer.yml down
