80ea673ebb3e7d07572bfdc729cd65d70656b2f2
monte97 <francesco.montelli1997@gmail.com>
2021-09-10 12:43:00 +0000

    fix exp launch

--- testScripts/testDeviceOfficial.sh
+++ testScripts/testDeviceOfficial.sh
@@ -1,5 +1,5 @@
-./launchRemoteExperiment.sh 1 200 1 1000 0 0 1000 device
-./launchRemoteExperiment.sh 10 200 1 1000 0 0 1000 device
-./launchRemoteExperiment.sh 100 200 1 1000 0 0 1000 device
-./launchRemoteExperiment.sh 125 200 1 1000 0 0 1000 device
-./launchRemoteExperiment.sh 150 200 1 1000 0 0 1000 device
+./launchRemoteExperiment.sh 1 200 1 4500 0 0 0 device
+./launchRemoteExperiment.sh 10 200 1 4500 0 0 0 device
+./launchRemoteExperiment.sh 100 200 1 4500 0 0 0 device
+#./launchRemoteExperiment.sh 125 200 1 1000 0 0 5000 device
+#./launchRemoteExperiment.sh 150 200 1 1000 0 0 5000 device
