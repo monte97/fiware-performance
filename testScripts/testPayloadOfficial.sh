17ca4afcbca73e8d5cb9e78612c8e7d246dba1af
monte97 <francesco.montelli1997@gmail.com>
2021-09-10 12:43:23 +0000

    adjust durtion to 15min per run

--- testScripts/testPayloadOfficial.sh
+++ testScripts/testPayloadOfficial.sh
@@ -1,6 +1,6 @@
-./launchRemoteExperiment.sh 50 200 1 1000 0 0 1000 payload
-./launchRemoteExperiment.sh 50 200 1 1000 0 0 10000 payload
-./launchRemoteExperiment.sh 50 200 1 1000 0 0 100000 payload
-./launchRemoteExperiment.sh 5 200 1 1000 0 0 1000 payload
-./launchRemoteExperiment.sh 5 200 1 1000 0 0 10000 payload
-./launchRemoteExperiment.sh 5 200 1 1000 0 0 100000 payload
+./launchRemoteExperiment.sh 50 200 1 4500 0 0 1000 payload
+./launchRemoteExperiment.sh 50 200 1 4500 0 0 10000 payload
+./launchRemoteExperiment.sh 50 200 1 4500 0 0 100000 payload
+./launchRemoteExperiment.sh 5 200 1 4500 0 0 1000 payload
+./launchRemoteExperiment.sh 5 200 1 4500 0 0 10000 payload
+./launchRemoteExperiment.sh 5 200 1 4500 0 0 100000 payload
