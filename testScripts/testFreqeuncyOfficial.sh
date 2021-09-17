131a0a050d34e123531d3a1eabf55e9c24fc6731
monte97 <francesco.montelli1997@gmail.com>
2021-09-17 13:41:57 +0000

    clean

--- testScripts/testFreqeuncyOfficial.sh
+++ testScripts/testFreqeuncyOfficial.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-SET_NAME=$1
-./launchRemoteExperiment.sh 5 1000 1 1000 0 0 1000 ${SET_NAME}
-./launchRemoteExperiment.sh 5 100 1 9000 0 0 1000 ${SET_NAME}
-./launchRemoteExperiment.sh 5 40 1 22500 0 0 1000 ${SET_NAME}
-./launchRemoteExperiment.sh 5 20 1 45000 0 0 1000 ${SET_NAME}
-./launchRemoteExperiment.sh 5 10 1 90000 0 0 1000 ${SET_NANE}
-./launchRemoteExperiment.sh 5 8 1 112500 0 0 1000 ${SET_NAME}
+EXP_NAME=$1
+./launchRemoteExperiment.sh 5 1000 1 1000 0 0 1000 ${EXP_NAME}
+./launchRemoteExperiment.sh 5 100 1 9000 0 0 1000 ${EXP_NAME}
+./launchRemoteExperiment.sh 5 40 1 22500 0 0 1000 ${EXP_NAME}
+./launchRemoteExperiment.sh 5 20 1 45000 0 0 1000 ${EXP_NAME}
+./launchRemoteExperiment.sh 5 10 1 90000 0 0 1000 ${EXP_NANE}
+./launchRemoteExperiment.sh 5 8 1 112500 0 0 1000 ${EXP_NAME}
 
--- testScripts/testSubsOfficial.sh
+++ testScripts/testSubsOfficial.sh
@@ -3,6 +3,3 @@ EXP_NAME=$1
 ./launchRemoteExperiment.sh 50 200 1 4500 0 0 1000 ${EXP_NAME}
 ./launchRemoteExperiment.sh 50 200 10 4500 0 0 1000 ${EXP_NAME}
 ./launchRemoteExperiment.sh 50 200 100 4500 0 0 1000 ${EXP_NAME}
-#./launchRemoteExperiment.sh 50 200 1 4500 0 0 1000 subsV2
-#./launchRemoteExperiment.sh 50 200 10 4500 0 0 1000 subsV2
-#./launchRemoteExperiment.sh 50 200 100 4500 0 0 1000 subsV2
