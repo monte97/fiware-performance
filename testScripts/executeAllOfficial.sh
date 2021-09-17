aa5a75ee3e1a23c3c3e9617d5c4f11a813af7d35
monte97 <francesco.montelli1997@gmail.com>
2021-09-17 13:47:49 +0000

    automation, create a folder for each set of experiments

--- testScripts/executeAllOfficial.sh
+++ testScripts/executeAllOfficial.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 SET_ID=$1
+mkdir master_logs/${SET_ID}
+
 ./testScripts/testDeviceOfficial.sh "DEVICE_${SET_ID}"
 ./testScripts/testFreqeuncyOfficial.sh "FREQUENCY_${SET_ID}"
 ./testScripts/testPayloadOfficial.sh "PAYLOAD_${SET_ID}"
 ./testScripts/testSubsOfficial.sh "SUB_${SET_ID}"
+
+mv master_logs/*_${SET_ID} master_logs/${SET_ID}
