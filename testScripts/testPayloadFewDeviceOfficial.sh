a2feff3e89c241583edff92ab9b23042831c5981
monte97 <francesco.montelli1997@gmail.com>
2021-09-24 16:02:19 +0000

    remove unused script tests

--- testScripts/testPayloadFewDeviceOfficial.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-EXP_NAME=$1
-timeout 60m ./launchRemoteExperiment.sh 5 200 1 4500 0 0 1000 ${EXP_NAME}
-timeout 60m ./launchRemoteExperiment.sh 5 200 1 4500 0 0 10000 ${EXP_NAME}
-timeout 60m ./launchRemoteExperiment.sh 5 200 1 4500 0 0 50000 ${EXP_NAME}
-timeout 60m ./launchRemoteExperiment.sh 5 200 1 4500 0 0 100000 ${EXP_NAME}
-timeout 60m ./launchRemoteExperiment.sh 5 200 1 4500 0 0 175000 ${EXP_NAME}
-timeout 60m ./launchRemoteExperiment.sh 5 200 1 4500 0 0 150000 ${EXP_NAME}
