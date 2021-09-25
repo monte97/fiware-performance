45f21276c4fe9ae644a135958bed537d411b3ca3
monte97 <francesco.montelli1997@gmail.com>
2021-09-25 13:56:09 +0000

    fix debug

--- supportScripts/debug.sh
+++ supportScripts/debug.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SET_ID="debug"
+SET_ID="debugv0"
 
 if [ -f .env ]; then
   export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
@@ -9,7 +9,7 @@ rm ${LOG_FILE}
 touch ${LOG_FILE}
 mkdir ${DATA_FOLDER}/${SET_ID}
 
-./supportScripts/launchRemoteExperiment.sh 2 200 1 10 0 0 1000 "debug" ${SET_ID}
+./supportScripts/launchRemoteExperiment.sh 2 200 1 10 0 0 1000 "debug_${SET_ID}" ${SET_ID}
 
 mv ${DATA_FOLDER}/*_${SET_ID} ${DATA_FOLDER}/${SET_ID}
 
