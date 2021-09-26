89f691f9b2ce857e584e9292ecac72d7544b67a2
monte97 <francesco.montelli1997@gmail.com>
2021-09-26 00:46:04 +0000

    typo

--- testScripts/executeAllOfficial.sh
+++ testScripts/executeAllOfficial.sh
@@ -15,7 +15,7 @@ mkdir -p ${DATA_FOLDER}/${SET_ID}
 ./testScripts/testPayloadOfficial.sh "PAYLOAD_${SET_ID}" ${SET_ID}
 ./testScripts/testSubsOfficial.sh "SUB_${SET_ID}" ${SET_ID}
 
-mv ${DATA_FOLDER}/*_${SET_ID} ${DAT_FOLDER}/${SET_ID}
+mv ${DATA_FOLDER}/*_${SET_ID} ${DATA_FOLDER}/${SET_ID}
 
 ./supportScripts/downloadMissing.sh ${LOG_FILE}
 ./supportScripts/stopAll.sh
