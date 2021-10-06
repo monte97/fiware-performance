513ea7b2b549415059ddcc4f1f64d76e396d3847
Francesco Montelli <francesco.montelli1997@gmail.com>
2021-10-06 15:30:05 +0200

    Update testPayloadOfficial.sh

--- testScripts/testPayloadOfficial.sh
+++ testScripts/testPayloadOfficial.sh
@@ -4,5 +4,7 @@ EXP_SET=$2
 timeout 60m ./supportScripts/launchRemoteExperiment.sh 50 200 1 4500 0 0 1000 ${EXP_NAME} ${EXP_SET}
 timeout 60m ./supportScripts/launchRemoteExperiment.sh 50 200 1 4500 0 0 10000 ${EXP_NAME} ${EXP_SET}
 timeout 60m ./supportScripts/launchRemoteExperiment.sh 50 200 1 4500 0 0 50000 ${EXP_NAME} ${EXP_SET}
+timeout 60m ./supportScripts/launchRemoteExperiment.sh 50 200 1 4500 0 0 65000 {EXP_NAME} ${EXP_SET}
+timeout 60m ./supportScripts/launchRemoteExperiment.sh 50 200 1 4500 0 0 80000 {EXP_NAME} ${EXP_SET}
 timeout 60m ./supportScripts/launchRemoteExperiment.sh 50 200 1 4500 0 0 100000 ${EXP_NAME} ${EXP_SET}
 
